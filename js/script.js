31de30bc959ef0b9187884ae265fcd92a74b8f96
Krzysztof Kalus <eumo37@gmail.com>
2023-04-25 23:52:12 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+
     const render = () => {
         let htmlString = "";
 
@@ -17,21 +31,23 @@
             htmlString += `
                 <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}
                 >
+                <button class = "js-remove">usuń</button>;
                     ${task.content}
                 </li>
                 `;
-        }
+        };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButton = document.querySelectorAll(".js-remove");
+
+        removeButton.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        })
-
-        render();
-    }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -40,10 +56,10 @@
 
         if (newTaskContent === "") {
             return;
-        }
+        };
 
         addNewTask(newTaskContent);
-    }
+    };
 
     const init = () => {
         render();
@@ -54,4 +70,4 @@
     };
 
     init();
-}
+};
